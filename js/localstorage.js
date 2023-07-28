7dfe4d32d0f1100197c993b5a09b0bd22828e04d
maximoortelli <maximoortelli@hotmail.com>
2023-07-28 14:35:41 -0300

    Update files
    Update index.html
    Update localstorage.js

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,34 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  const inputs = form.querySelectorAll('input, textarea');
+function localData() {
+  const localData = {
+    name: document.getElementById('savename').value,
+    email: document.getElementById('saveemail').value,
+    message: document.querySelector('textarea').value,
+  };
+  localStorage.setItem('localData', JSON.stringify(localData));
+}
 
-  // Function to save data to local storage
-  function saveData() {
-    const data = {};
-    inputs.forEach((input) => {
-      data[input.name] = input.value;
-    });
-    localStorage.setItem('formData', JSON.stringify(data));
+const Inputs = document.querySelectorAll('#form input, #form textarea');
+Inputs.forEach((input) => {
+  input.addEventListener('input', localData);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  let localData = localStorage.getItem('localData');
+  if (localData) {
+    localData = JSON.parse(localData);
+    document.getElementById('savename').value = localData.name || '';
+    document.getElementById('saveemail').value = localData.email || '';
+    document.querySelector('textarea').value = localData.message || '';
   }
-
-  // Function to load data from local storage
-  function loadData() {
-    const data = JSON.parse(localStorage.getItem('formData'));
-    if (data) {
-      inputs.forEach((input) => {
-        // eslint-disable-next-line no-prototype-builtins
-        if (data.hasOwnProperty(input.name)) {
-          input.value = data[input.name];
-        }
-      });
-    }
-  }
-
-  // Event listener for input changes
-  inputs.forEach((input) => {
-    input.addEventListener('input', saveData);
-  });
-
-  // Load data when the page is loaded
-  window.addEventListener('load', loadData);
 });
